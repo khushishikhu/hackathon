08c5bf3ab5e0fc3d91a61027cddf1fe1b625e4ab
Khushi Gautam <dpskhu13108@gmail.com>
2023-07-02 15:14:54 +0530

    Commit

--- app/pages/secret.js
+++ app/pages/secret.js
@@ -3,6 +3,10 @@ import { useSession } from "next-auth/react";
 import PostCard from "../components/Postcard";
 import styles from '../styles/Home.module.css';
 import dynamic from "next/dynamic";
+import Navbar from '../components/Navbar';
+import Link from "next/link";
+import { Grid } from "@mui/material";
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 function Secret({posts}){
     const{data: session,status}= useSession();
@@ -12,7 +16,7 @@ function Secret({posts}){
 
     useEffect(()=>{
         const fetchData = async()=>{
-            const res = await fetch("/api/secret");
+            const res = await fetch("/api/secret/index/");
             const json = await res.json();
             if(json.content){
                 setContent(json.content)
@@ -25,31 +29,48 @@ function Secret({posts}){
 
     if(!session){
         return(
+            <>
+            <Navbar></Navbar>
             <main>
                 <div>
                     <h1>You aren't sign in. Please signed in first</h1>
                 </div>
             </main>
+            </>
         )
     }
     return(
+        <>
+        <Navbar></Navbar>
         <main>
-            <div>
-                <h1>Protected Pages</h1>
+            <div className={styles.container}>
+                <h1>Add the Finder Information</h1>
+                
+                <br></br>
                 
             </div>
             <div className={styles.container}>
                     {posts.length === 0 ? (
                         <h2>No added posts</h2>
                     ) : (
-                        <ul>
+                        <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        
                             {posts.map((post, i) => (
+                                <Grid item xs={2} sm={4} md={4} key={i}>
                                 <PostCard post={post} key={i} />
+                                </Grid>
                             ))}
-                        </ul>
+                            <Link href="/AddPost">
+                <AddCircleOutlineOutlinedIcon sx={{ fontSize: 40 }} ></AddCircleOutlineOutlinedIcon>
+                Add the Post</Link>
+                        
+                        </Grid>
+                        
                     )}
+                    
                 </div>
         </main>
+        </>
     )
 
 }
